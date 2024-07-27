62e8c4ae2381b4f90ffbd1dad528f83763907481
thient27 <Denkhotieu1@gmail.com>
2024-07-28 00:24:48 +0700

    unity to react

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,36 @@
 import React from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
+import './App.css';
+import logo from "./Assets/logo.png"; 
 
 const UNITY_PATH = "unity/Build";
-const UNITY_NAME = "unity";
+const UNITY_NAME = "flying-baby-dragon-webgl-deploy-test";
 
 function App() {
-  const { unityProvider } = useUnityContext({
+  const { unityProvider, isLoaded, loadingProgression } = useUnityContext({
     loaderUrl: `${UNITY_PATH}/${UNITY_NAME}.loader.js`,
     dataUrl: `${UNITY_PATH}/${UNITY_NAME}.data`,
     frameworkUrl: `${UNITY_PATH}/${UNITY_NAME}.framework.js`,
     codeUrl: `${UNITY_PATH}/${UNITY_NAME}.wasm`,
   });
 
+  const loadingPercentage = Math.round(loadingProgression * 100);
+
   return (
-    <Unity unityProvider={unityProvider} style={{ width: 800, height: 600 }} />
+    <div id="unity-container" className="unity-desktop">
+      {!isLoaded && (
+        <div id="loading-cover">
+          <div id="unity-loading-bar">
+            <div id="unity-logo"><img src={logo} alt="Unity Logo" /></div>
+            <div id="unity-progress-bar-empty">
+              <div id="unity-progress-bar-full" style={{ width: `${loadingPercentage}%` }}></div>
+            </div>
+            <div className="spinner"></div>
+          </div>
+        </div>
+      )}
+      <Unity className="unity" unityProvider={unityProvider} />
+    </div>
   );
 }
 
