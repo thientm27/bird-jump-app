a0e8aef470ba3ce5aef9a8a6a10ac0b0090fe5cb
thient27 <Denkhotieu1@gmail.com>
2024-07-28 14:16:12 +0700

    Tructual

---
+++ src/Page/Ton/Ton.tsx
@@ -0,0 +1,13 @@
+import React, { } from "react";
+
+
+const Ton: React.FC = () => {
+
+  return (
+    <div>
+      Ton here
+   </div>
+    );
+};
+
+export default Ton;
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Ton from './Page/Ton/Ton';
 import Game from './Page/Game/Game';
 import reportWebVitals from './reportWebVitals';
 
@@ -10,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Game />
+    <Ton />
   </React.StrictMode>
 );
 
