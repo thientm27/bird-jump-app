8018f70c907290573287b0b132c44afc70fe1f66
thient27 <Denkhotieu1@gmail.com>
2024-07-28 16:13:33 +0700

    ton login

--- src/Page/Ton/Ton.tsx
+++ src/Page/Ton/Ton.tsx
@@ -6,14 +6,27 @@ const Ton: React.FC = () => {
   return (
     
         <TonConnectUIProvider manifestUrl="https://raw.githubusercontent.com/thientm27/bird-jump-app/main/tonconnect-manifest.json" 
-            actionsConfiguration={{twaReturnUrl: 'https://thientm27.github.io/bird-jump-app/'}}
-        >
+        walletsListConfiguration={{
+            includeWallets: [
+                {
+                    appName: "telegram-wallet",
+                    name: "Wallet",
+                    imageUrl: "https://wallet.tg/images/logo-288.png",
+                    aboutUrl: "https://wallet.tg/",
+                    universalLink: "https://t.me/wallet/start",
+                    bridgeUrl: "https://bridge.tonapi.io/bridge",
+                    platforms: ["ios", "android", "macos", "windows", "linux"]
+                }
+            ]
+        }}
+        actionsConfiguration={{twaReturnUrl: 'https://t.me/thientm_bot/birdjump'}}
+       >
             
         <header>
             <span>My App with React UI</span>
             <TonConnectButton className="my-button-class" style={{ float: "right" }}/>
         </header>
-        
+        <div>https://t.me/thientm_bot?start=webapp</div>
         <div>My app</div>
          
         </TonConnectUIProvider> 
