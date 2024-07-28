c94165fee0192b67b61b531aec3dbafa94750b98
thient27 <Denkhotieu1@gmail.com>
2024-07-28 14:31:36 +0700

    Ton

--- src/Page/Ton/Ton.tsx
+++ src/Page/Ton/Ton.tsx
@@ -1,12 +1,22 @@
 import React, { } from "react";
-
+import { TonConnectButton, TonConnectUIProvider } from '@tonconnect/ui-react';
 
 const Ton: React.FC = () => {
 
   return (
-    <div>
-      Ton here
-   </div>
+    
+        <TonConnectUIProvider manifestUrl="#"   actionsConfiguration={{
+            twaReturnUrl: 'https://t.me/thientm_denk'
+        }}>
+            
+        <header>
+            <span>My App with React UI</span>
+            <TonConnectButton className="my-button-class" style={{ float: "right" }}/>
+        </header>
+        
+        <div>My app</div>
+         
+        </TonConnectUIProvider> 
     );
 };
 
